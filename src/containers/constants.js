08e555488139027b3a98d0bb68b9d3a15705a761
superstar <83311833+clever-web@users.noreply.github.com>
2022-09-24 16:12:58 +0300

    Create constants.js

---
+++ src/containers/constants.js
@@ -0,0 +1,47 @@
+export const CALCULATOR_TABLE = [
+	{
+		timeframe: '1D',
+		roi: '0.39%',
+		reward: { line1: '0.185 FORM', line2: '= $ 3.896' },
+	},
+	{
+		timeframe: '7D',
+		roi: '2.73%',
+		reward: { line1: '1.296 FORM', line2: '= $ 27.274' },
+	},
+	{
+		timeframe: '30D',
+		roi: '11.69%',
+		reward: { line1: '5.556 FORM', line2: '= $ 116.889' },
+	},
+	{
+		timeframe: '1Y',
+		roi: '313.46%',
+		reward: { line1: '5.556 FORM', line2: '= $ 3,34.59' },
+	},
+];
+
+export const REMINDER_STYLE = {
+	DARK: {
+		BOX_1: {
+			BORDER: { border: '1px solid #fff' },
+			TITLE: { color: '#8cffcc' },
+		},
+		BOX_2: {
+			BORDER: { border: '1px solid rgba(255, 255, 255, 0.2)' },
+			TEXT: { color: '#fff', opacity: '0.5' },
+			LINK: { color: '#8cffcc' },
+		},
+	},
+	LIGHT: {
+		BOX_1: {
+			BORDER: { border: '1px solid rgba(7, 6, 24, 0.2)' },
+			TITLE: { color: '#070618' },
+		},
+		BOX_2: {
+			BORDER: { border: '1px solid rgba(7, 6, 24, 0.2)' },
+			TEXT: { color: '#070618' },
+			LINK: { color: '#070618' },
+		},
+	},
+};
