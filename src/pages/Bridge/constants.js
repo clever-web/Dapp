d43c66d4c4a820e84274796967283b70d4290d2b
superstar <83311833+clever-web@users.noreply.github.com>
2022-09-28 16:18:10 +0300

    Create constants.js

---
+++ src/pages/Bridge/constants.js
@@ -0,0 +1,12 @@
+import FORM_BNB_Icon from "ASSETS/images/pages/bridge/form_bnb_chain.svg";
+import FORM_ETH_Icon from "ASSETS/images/pages/bridge/form_eth_chain.svg";
+
+export const options = [
+  { name: "BNB", icon: FORM_BNB_Icon },
+  { name: "ETH", icon: FORM_ETH_Icon },
+];
+
+export const ANYSWAP_CONFIG = {
+  SWAP_TO_BEP_ADDRESS: "0x533e3c0e6b48010873B947bddC4721b1bDFF9648",
+  ANY_FORM_ADDRESS_BSC: "0x25a528af62e56512a19ce8c3cab427807c28cc19",
+};
