7bb53726bcd0c6ace2db9f09d640311a91c91460
superstar <83311833+clever-web@users.noreply.github.com>
2022-10-01 12:54:48 +0300

    Create constants.js

---
+++ src/pages/Pool/constants.js
@@ -0,0 +1,18 @@
+import FORMTokenIcon from 'ASSETS/images/tokens/form.svg';
+
+export const TAB_NAMES = {
+	STAKE: 'pools-stake',
+	UNSTAKE: 'pools-unstake',
+	APPROVAL: 'pools-approval',
+};
+
+export const tokens = [
+	{
+		name: 'FORM',
+		description: 'Formation Fi',
+		amount: 0,
+		icon: FORMTokenIcon,
+	},
+];
+
+export const AMOUNT_TO_REMOVE = ['25', '50', '75', '100'];
