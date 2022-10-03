c2cdf3861bbf75da8be34cfc14a9bc8b3f79f77e
superstar <83311833+clever-web@users.noreply.github.com>
2022-10-03 09:22:15 +0300

    Create Header.jsx

---
+++ src/pages/StableSwap/components/SwapTab/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+	return (
+		<div className='swap-tab__header'>
+			<p className='font-size-14 font-weight-500'>
+				Trade tokens in an instant
+			</p>
+		</div>
+	);
+};
+
+export default Header;
