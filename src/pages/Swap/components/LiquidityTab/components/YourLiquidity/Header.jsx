bdabbb88189d9643f014a091e57844835401d676
superstar <83311833+clever-web@users.noreply.github.com>
2022-10-05 17:12:59 +0300

    Create Header.jsx

---
+++ src/pages/Swap/components/LiquidityTab/components/YourLiquidity/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="liquidity-tab__your-liquidity__header">
+      <p className="font-size-14 font-weight-500">Your Liquidity</p>
+    </div>
+  );
+};
+
+export default Header;
