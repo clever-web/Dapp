c28ccd5620af423348258f967cc5a6db80659574
superstar <83311833+clever-web@users.noreply.github.com>
2022-10-05 17:13:32 +0300

    Create BetMarketHead.jsx

---
+++ src/pages/V1alpha/components/BetMarketHead.jsx
@@ -0,0 +1,8 @@
+const BetMarketHead = () => {
+	
+	return <div className='v1alpha__alpha__rightBottom_title'>
+                Beating Market &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <b className="font-size-20">+3</b>
+            </div>
+};
+
+export default BetMarketHead;
