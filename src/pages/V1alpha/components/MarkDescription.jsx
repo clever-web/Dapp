3bbec55ab5ba68d5c3051ad31a046b19128b133a
superstar <83311833+clever-web@users.noreply.github.com>
2022-10-09 17:56:44 +0300

    Create MarkDescription.jsx

---
+++ src/pages/V1alpha/components/MarkDescription.jsx
@@ -0,0 +1,9 @@
+const MarkDescription = ({ text, amount }) => {
+	
+	return  <div className = "v1alpha__alpha__content__left__markDescription">
+                <p className="font-size-12 thesis">THESIS</p>
+                <p className="font-size-12">Pure Growth (catch the new fast-moving opportunities). Cross-chain yield farming strategies with liqudidity mining programs offered by leading DeFi projects to maximize APYs.</p>
+            </div>
+};
+
+export default MarkDescription;
