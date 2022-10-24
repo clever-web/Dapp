cc8ae9c513be06de2b5b3acb118da62d7d9b74f6
superstar <83311833+clever-web@users.noreply.github.com>
2022-10-24 21:08:46 +0300

    Update Bg.jsx

--- src/containers/Bg.jsx
+++ src/containers/Bg.jsx
@@ -1,17 +1,17 @@
-import { useLocation } from "react-router";
-const Bg = () => {
-	let location = useLocation();
-	if(location.pathname != '/')
-		return(
-			<div className='linear-bg__container'>
-		
-				<div className='linear-bg__container--left'>
+	import { useLocation } from "react-router";
+	const Bg = () => {
+		let location = useLocation();
+		if(location.pathname != '/')
+			return(
+				<div className='linear-bg__container'>
+			
+					<div className='linear-bg__container--left'>
+					</div>
+					<div className='linear-bg__container--right'></div>
 				</div>
-				<div className='linear-bg__container--right'></div>
-			</div>
-		);
-	else
-		return(<></>);
-}
+			);
+		else
+			return(<></>);
+	}
 
-export default Bg;
+	export default Bg;
