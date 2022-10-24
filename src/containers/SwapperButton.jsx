da8f0c9d2976e0a0534aad47eb861652b52bb4de
superstar <83311833+clever-web@users.noreply.github.com>
2022-10-24 21:09:39 +0300

    Update SwapperButton.jsx

--- src/containers/SwapperButton.jsx
+++ src/containers/SwapperButton.jsx
@@ -1,54 +1,54 @@
-import Image from 'COMPONENTS/Image';
-import ArrowUp from 'ASSETS/images/common/arrow-up.svg';
-import ArrowDown from 'ASSETS/images/common/arrow-down.svg';
-import SwapArrows from 'ASSETS/images/actions/arrows.svg';
+	import Image from 'COMPONENTS/Image';
+	import ArrowUp from 'ASSETS/images/common/arrow-up.svg';
+	import ArrowDown from 'ASSETS/images/common/arrow-down.svg';
+	import SwapArrows from 'ASSETS/images/actions/arrows.svg';
 
-const SwapperButton = ({
-	icon,
-	iconWidth,
-	iconHeight,
-	btnClasses = '',
-	classes,
-	onClick,
-}) => (
-	<div className='button-swapper'>
-		<button
-			type='button'
-			onClick={onClick}
-			className={`button-swapper__button ${btnClasses}`}
-			style={{border: '1px solid #99ffcc'}}
-		>
-			{icon ? (
-				<Image
-					light={icon || SwapArrows}
-					dark={icon || SwapArrows}
-					alt='Swap'
-					w={iconWidth || '42'}
-					h={iconHeight || '42'}
-					classes={classes}
-				/>
-			) : (
-				<>
+	const SwapperButton = ({
+		icon,
+		iconWidth,
+		iconHeight,
+		btnClasses = '',
+		classes,
+		onClick,
+	}) => (
+		<div className='button-swapper'>
+			<button
+				type='button'
+				onClick={onClick}
+				className={`button-swapper__button ${btnClasses}`}
+				style={{border: '1px solid #99ffcc'}}
+			>
+				{icon ? (
 					<Image
-						light={ArrowUp}
-						dark={ArrowUp}
+						light={icon || SwapArrows}
+						dark={icon || SwapArrows}
 						alt='Swap'
-						w='10'
-						h='16'
-						classes='button-swapper__button__arrow-up'
+						w={iconWidth || '42'}
+						h={iconHeight || '42'}
+						classes={classes}
 					/>
-					<Image
-						light={ArrowDown}
-						dark={ArrowDown}
-						alt='Swap'
-						w='10'
-						h='16'
-						classes='button-swapper__button__arrow-down'
-					/>
-				</>
-			)}
-		</button>
-	</div>
-);
+				) : (
+					<>
+						<Image
+							light={ArrowUp}
+							dark={ArrowUp}
+							alt='Swap'
+							w='10'
+							h='16'
+							classes='button-swapper__button__arrow-up'
+						/>
+						<Image
+							light={ArrowDown}
+							dark={ArrowDown}
+							alt='Swap'
+							w='10'
+							h='16'
+							classes='button-swapper__button__arrow-down'
+						/>
+					</>
+				)}
+			</button>
+		</div>
+	);
 
-export default SwapperButton;
+	export default SwapperButton;
