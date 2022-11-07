2b928d05c3ca9c292ebdffc87137fb17b93a152d
superstar <83311833+clever-web@users.noreply.github.com>
2022-11-07 04:34:07 +0200

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,28 +1,28 @@
-import ReactDOM from 'react-dom';
-import Backdrop from './Backdrop';
+	import ReactDOM from 'react-dom';
+	import Backdrop from './Backdrop';
 
-const ModalOverlay = (props) => {
-	const content = (
-		<div className={`modal ${props.classes || ''}`}>{props.children}</div>
-	);
-	return ReactDOM.createPortal(
-		content,
-		document.getElementById('modal-hook')
-	);
-};
+	const ModalOverlay = (props) => {
+		const content = (
+			<div className={`modal ${props.classes || ''}`}>{props.children}</div>
+		);
+		return ReactDOM.createPortal(
+			content,
+			document.getElementById('modal-hook')
+		);
+	};
 
-const Modal = (props) => {
-	return (
-		<>
-			{props.show && (
-				<Backdrop
-					onClick={props.onCancel}
-					classes={props.backdropClasses}
-				/>
-			)}
-			{props.show && <ModalOverlay {...props} />}
-		</>
-	);
-};
+	const Modal = (props) => {
+		return (
+			<>
+				{props.show && (
+					<Backdrop
+						onClick={props.onCancel}
+						classes={props.backdropClasses}
+					/>
+				)}
+				{props.show && <ModalOverlay {...props} />}
+			</>
+		);
+	};
 
-export default Modal;
+	export default Modal;
