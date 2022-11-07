8648d2799f2de253d685ec82568896aa08ccd551
superstar <83311833+clever-web@users.noreply.github.com>
2022-11-07 04:33:21 +0200

    Update Image.jsx

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,18 +1,18 @@
-import { useContext } from 'react';
-import { ThemeContext } from 'CONTEXT/theme-context';
+	import { useContext } from 'react';
+	import { ThemeContext } from 'CONTEXT/theme-context';
 
-const Image = ({ light, dark, alt, w, h, classes = '' }) => {
-	const { theme } = useContext(ThemeContext);
+	const Image = ({ light, dark, alt, w, h, classes = '' }) => {
+		const { theme } = useContext(ThemeContext);
 
-	return (
-		<img
-			src={theme === 'theme-light' ? light : dark}
-			alt={alt}
-			width={w}
-			height={h}
-			className={classes}
-		/>
-	);
-};
+		return (
+			<img
+				src={theme === 'theme-light' ? light : dark}
+				alt={alt}
+				width={w}
+				height={h}
+				className={classes}
+			/>
+		);
+	};
 
-export default Image;
+	export default Image;
