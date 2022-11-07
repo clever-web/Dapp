2f1ce49c3f6484329651d4b027d211cda3728107
superstar <83311833+clever-web@users.noreply.github.com>
2022-11-07 04:33:19 +0200

    Update ButtonClose.jsx

--- src/components/ButtonClose.jsx
+++ src/components/ButtonClose.jsx
@@ -1,18 +1,18 @@
-import Image from 'COMPONENTS/Image';
+	import Image from 'COMPONENTS/Image';
 
-import CancelDarkIcon from 'ASSETS/images/actions/dark/cancel.svg';
-import CancelLightIcon from 'ASSETS/images/actions/light/cancel.svg';
+	import CancelDarkIcon from 'ASSETS/images/actions/dark/cancel.svg';
+	import CancelLightIcon from 'ASSETS/images/actions/light/cancel.svg';
 
-const ButtonClose = ({ onClick, classes = '' }) => (
-	<button type='button' className={`btn-icon ${classes}`} onClick={onClick}>
-		<Image
-			light={CancelLightIcon}
-			dark={CancelDarkIcon}
-			alt='Close'
-			w='15'
-			h='15'
-		/>
-	</button>
-);
+	const ButtonClose = ({ onClick, classes = '' }) => (
+		<button type='button' className={`btn-icon ${classes}`} onClick={onClick}>
+			<Image
+				light={CancelLightIcon}
+				dark={CancelDarkIcon}
+				alt='Close'
+				w='15'
+				h='15'
+			/>
+		</button>
+	);
 
-export default ButtonClose;
+	export default ButtonClose;
