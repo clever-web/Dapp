a6a56cd62891656a3ebf062381483162de98583b
superstar <83311833+clever-web@users.noreply.github.com>
2022-11-07 04:34:05 +0200

    Update MigrationReminder.jsx

--- src/components/MigrationReminder.jsx
+++ src/components/MigrationReminder.jsx
@@ -1,54 +1,54 @@
-import { useContext } from "react";
-import { ThemeContext } from "CONTEXT/theme-context";
-import { REMINDER_STYLE } from "containers/constants";
+  import { useContext } from "react";
+  import { ThemeContext } from "CONTEXT/theme-context";
+  import { REMINDER_STYLE } from "containers/constants";
 
-const MigrationReminder = () => {
-  const { theme } = useContext(ThemeContext);
+  const MigrationReminder = () => {
+    const { theme } = useContext(ThemeContext);
 
-  return (
-    <div className="reminder mt-4">
-      <div
-        className="reminder__info--2"
-        style={
-          theme === "theme-light"
-            ? REMINDER_STYLE.LIGHT.BOX_2.BORDER
-            : REMINDER_STYLE.DARK.BOX_2.BORDER
-        }
-      >
-        <p className="txt-center font-size-14 line-height-4">
-          <span
-            style={
-              theme === "theme-light"
-                ? REMINDER_STYLE.LIGHT.BOX_2.TEXT
-                : REMINDER_STYLE.DARK.BOX_2.TEXT
-            }
-          >
-            Please contact{" "}
-          </span>
-          <a
-            href="mailto:support@formation.fi"
-            className="reminder__info--2__link"
-            style={
-              theme === "theme-light"
-                ? REMINDER_STYLE.LIGHT.BOX_2.LINK
-                : REMINDER_STYLE.DARK.BOX_2.LINK
-            }
-          >
-            support@formation.fi
-          </a>{" "}
-          <span
-            style={
-              theme === "theme-light"
-                ? REMINDER_STYLE.LIGHT.BOX_2.TEXT
-                : REMINDER_STYLE.DARK.BOX_2.TEXT
-            }
-          >
-            with any questions related to farms.
-          </span>
-        </p>
+    return (
+      <div className="reminder mt-4">
+        <div
+          className="reminder__info--2"
+          style={
+            theme === "theme-light"
+              ? REMINDER_STYLE.LIGHT.BOX_2.BORDER
+              : REMINDER_STYLE.DARK.BOX_2.BORDER
+          }
+        >
+          <p className="txt-center font-size-14 line-height-4">
+            <span
+              style={
+                theme === "theme-light"
+                  ? REMINDER_STYLE.LIGHT.BOX_2.TEXT
+                  : REMINDER_STYLE.DARK.BOX_2.TEXT
+              }
+            >
+              Please contact{" "}
+            </span>
+            <a
+              href="mailto:support@formation.fi"
+              className="reminder__info--2__link"
+              style={
+                theme === "theme-light"
+                  ? REMINDER_STYLE.LIGHT.BOX_2.LINK
+                  : REMINDER_STYLE.DARK.BOX_2.LINK
+              }
+            >
+              support@formation.fi
+            </a>{" "}
+            <span
+              style={
+                theme === "theme-light"
+                  ? REMINDER_STYLE.LIGHT.BOX_2.TEXT
+                  : REMINDER_STYLE.DARK.BOX_2.TEXT
+              }
+            >
+              with any questions related to farms.
+            </span>
+          </p>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
-export default MigrationReminder;
+  export default MigrationReminder;
