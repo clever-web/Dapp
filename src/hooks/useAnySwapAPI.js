8f6212d6b77d56169da20dd9c3a725a3c6b01e2a
superstar <83311833+clever-web@users.noreply.github.com>
2022-11-10 08:55:01 +0200

    Update useAnySwapAPI.js

--- src/hooks/useAnySwapAPI.js
+++ src/hooks/useAnySwapAPI.js
@@ -1,39 +1,39 @@
-import { useEffect, useState } from "react";
+  import { useEffect, useState } from "react";
 
-export const FetchStatus = {
-  NOT_FETCHED: "not-fetched",
-  SUCCESS: "success",
-  FAILED: "failed",
-};
+  export const FetchStatus = {
+    NOT_FETCHED: "not-fetched",
+    SUCCESS: "success",
+    FAILED: "failed",
+  };
 
-export const useAnySwapAPI = () => {
-  const { NOT_FETCHED, SUCCESS, FAILED } = FetchStatus;
+  export const useAnySwapAPI = () => {
+    const { NOT_FETCHED, SUCCESS, FAILED } = FetchStatus;
 
-  const [formBridgeData, setFormBridgeData] = useState({
-    data: null,
-    fetchStatus: NOT_FETCHED,
-  });
+    const [formBridgeData, setFormBridgeData] = useState({
+      data: null,
+      fetchStatus: NOT_FETCHED,
+    });
 
-  useEffect(() => {
-    const getFormBridgeData = async () => {
-      try {
-        const URL = "https://bridgeapi.anyswap.exchange/v2/serverInfo/56";
-        const response = await fetch(URL);
-        const data = await response.json();
-        setFormBridgeData({
-          data: data['formv5'] ?? null,
-          fetchStatus: SUCCESS,
-        });
-      } catch (err) {
-        setFormBridgeData({
-          data: null,
-          fetchStatus: FAILED,
-        });
-      }
-    };
+    useEffect(() => {
+      const getFormBridgeData = async () => {
+        try {
+          const URL = "https://bridgeapi.anyswap.exchange/v2/serverInfo/56";
+          const response = await fetch(URL);
+          const data = await response.json();
+          setFormBridgeData({
+            data: data['formv5'] ?? null,
+            fetchStatus: SUCCESS,
+          });
+        } catch (err) {
+          setFormBridgeData({
+            data: null,
+            fetchStatus: FAILED,
+          });
+        }
+      };
 
-    getFormBridgeData();
-  }, [FAILED, SUCCESS]);
+      getFormBridgeData();
+    }, [FAILED, SUCCESS]);
 
-  return formBridgeData;
-};
+    return formBridgeData;
+  };
