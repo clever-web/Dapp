730f7c63980365a0903b63842772bfd6909a106d
superstar <83311833+clever-web@users.noreply.github.com>
2022-11-11 07:47:20 +0200

    Update useToastPortal.js

--- src/hooks/useToastPortal.js
+++ src/hooks/useToastPortal.js
@@ -1,20 +1,20 @@
-import { useEffect, useState } from 'react';
-import { uuid } from 'SHARED/helpers';
+	import { useEffect, useState } from 'react';
+	import { uuid } from 'SHARED/helpers';
 
-export const useToastPortal = () => {
-	const [loaded, setLoaded] = useState(false);
-	const [portalId] = useState(`toast-portal-${uuid()}`);
+	export const useToastPortal = () => {
+		const [loaded, setLoaded] = useState(false);
+		const [portalId] = useState(`toast-portal-${uuid()}`);
 
-	useEffect(() => {
-		const div = document.createElement('div');
-		div.id = portalId;
-		div.style = 'position: fixed; top: 15px; right: 15px; z-index: 150;';
-		document.getElementsByTagName('body')[0].prepend(div);
+		useEffect(() => {
+			const div = document.createElement('div');
+			div.id = portalId;
+			div.style = 'position: fixed; top: 15px; right: 15px; z-index: 150;';
+			document.getElementsByTagName('body')[0].prepend(div);
 
-		setLoaded(true);
+			setLoaded(true);
 
-		return () => document.getElementsByTagName('body')[0].removeChild(div);
-	}, [portalId]);
+			return () => document.getElementsByTagName('body')[0].removeChild(div);
+		}, [portalId]);
 
-	return { loaded, portalId };
-};
+		return { loaded, portalId };
+	};
