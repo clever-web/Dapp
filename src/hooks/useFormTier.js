4de1781a7381bbf11e9d642ce029b04826c3da1c
superstar <83311833+clever-web@users.noreply.github.com>
2022-11-11 07:43:24 +0200

    Update useFormTier.js

--- src/hooks/useFormTier.js
+++ src/hooks/useFormTier.js
@@ -1,58 +1,58 @@
-import { useWeb3React } from "@web3-react/core";
-import { ethers } from "ethers";
-import { useEffect, useState } from "react";
+  import { useWeb3React } from "@web3-react/core";
+  import { ethers } from "ethers";
+  import { useEffect, useState } from "react";
 
-const SNAPSHOT = 2;
+  const SNAPSHOT = 2;
 
-export const FetchStatus = {
-  NOT_FETCHED: "not-fetched",
-  SUCCESS: "success",
-  FAILED: "failed",
-};
+  export const FetchStatus = {
+    NOT_FETCHED: "not-fetched",
+    SUCCESS: "success",
+    FAILED: "failed",
+  };
 
-export const TIERS = {
-  DEFAULT: "default",
-  SILVER: "silver",
-  GOLD: "gold",
-  DIAMOND: "diamond",
-  BLACK: "black",
-};
+  export const TIERS = {
+    DEFAULT: "default",
+    SILVER: "silver",
+    GOLD: "gold",
+    DIAMOND: "diamond",
+    BLACK: "black",
+  };
 
-export const useFormTier = () => {
-  const { NOT_FETCHED, SUCCESS, FAILED } = FetchStatus;
-  const [tierState, setTierState] = useState({
-    tier: null,
-    fetchStatus: NOT_FETCHED,
-  });
-  const { account, chainId } = useWeb3React();
+  export const useFormTier = () => {
+    const { NOT_FETCHED, SUCCESS, FAILED } = FetchStatus;
+    const [tierState, setTierState] = useState({
+      tier: null,
+      fetchStatus: NOT_FETCHED,
+    });
+    const { account, chainId } = useWeb3React();
 
-  useEffect(() => {
-    const fetchTier = async () => {
-      try {
-        const response = await fetch(
-          `/.netlify/functions/early-adopter-tier?address=${ethers.utils.getAddress(
-            account
-          )}&chainId=${chainId}&snapshot=${SNAPSHOT}`,
-        );
-        const data = await response.json();
+    useEffect(() => {
+      const fetchTier = async () => {
+        try {
+          const response = await fetch(
+            `/.netlify/functions/early-adopter-tier?address=${ethers.utils.getAddress(
+              account
+            )}&chainId=${chainId}&snapshot=${SNAPSHOT}`,
+          );
+          const data = await response.json();
+          setTierState({
+            tier: data?.data?.type ?? null,
+            fetchStatus: SUCCESS,
+          });
+        } catch (error) {
+          setTierState({ tier: null, fetchStatus: FAILED });
+        }
+      };
+
+      if (account && chainId) {
+        fetchTier();
+      } else {
         setTierState({
-          tier: data?.data?.type ?? null,
-          fetchStatus: SUCCESS,
+          balance: null,
+          fetchStatus: NOT_FETCHED,
         });
-      } catch (error) {
-        setTierState({ tier: null, fetchStatus: FAILED });
       }
-    };
+    }, [FAILED, NOT_FETCHED, SUCCESS, account, chainId]);
 
-    if (account && chainId) {
-      fetchTier();
-    } else {
-      setTierState({
-        balance: null,
-        fetchStatus: NOT_FETCHED,
-      });
-    }
-  }, [FAILED, NOT_FETCHED, SUCCESS, account, chainId]);
-
-  return tierState;
-};
+    return tierState;
+  };
