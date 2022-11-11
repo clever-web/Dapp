8e091b494cf6f2c35c67ce4ccd84dfb0286dd5af
superstar <83311833+clever-web@users.noreply.github.com>
2022-11-11 07:43:31 +0200

    Update useMediaQuery.js

--- src/hooks/useMediaQuery.js
+++ src/hooks/useMediaQuery.js
@@ -1,21 +1,21 @@
-import { useState, useEffect } from 'react';
+	import { useState, useEffect } from 'react';
 
-export function useMediaQuery(query) {
-	const [matches, setMatches] = useState(false);
+	export function useMediaQuery(query) {
+		const [matches, setMatches] = useState(false);
 
-	useEffect(() => {
-		const media = window.matchMedia(query);
+		useEffect(() => {
+			const media = window.matchMedia(query);
 
-		if (media.matches !== matches) {
-			setMatches(media.matches);
-		}
+			if (media.matches !== matches) {
+				setMatches(media.matches);
+			}
 
-		const listener = () => setMatches(media.matches);
+			const listener = () => setMatches(media.matches);
 
-		media.addEventListener('change', listener);
+			media.addEventListener('change', listener);
 
-		return () => media.removeEventListener('change', listener);
-	}, [matches, query]);
+			return () => media.removeEventListener('change', listener);
+		}, [matches, query]);
 
-	return matches;
-}
+		return matches;
+	}
